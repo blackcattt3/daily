596a72ddb67ca103fc53a3f3ebb21673f1433733
Jaehee Roh <snowcat3b@gmail.com>
2025-09-24 10:07:50 +0900

    practice2

--- 9_24/rockScissorspaper/src/App.jsx
+++ 9_24/rockScissorspaper/src/App.jsx
@@ -2,45 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Box from './component/Box'
+import Box from "./component/Box.jsx"
 
 const choice = {
-    rock:{
-      name:"Rock",
-      img:"https://store.clickhole.com/cdn/shop/files/Untitleddesign_6.png?v=1693423886"
-    },
-    scissors:{
-      name:"Scissors",
-      img:"https://m.media-amazon.com/images/I/51QtmdQD-sL._UF894,1000_QL80_.jpg"    },
-    paper:{
-      name:"Paper",
-      img:"https://m.media-amazon.com/images/I/61OorFhm6SL._UF894,1000_QL80_.jpg"
-    }
+  scissors:{
+    name:"scissors",
+    img:"https://www.rapiddeliveryservices.in/uploads/webp/scisso_42831-.webp"
+  },
+  rock:{
+    name:"rock",
+    img:"https://store.clickhole.com/cdn/shop/files/Untitleddesign_6.png?v=1693423886"
+  },
+  paper:{
+    name:"paper",
+    img:"https://m.media-amazon.com/images/I/61OorFhm6SL._UF894,1000_QL80_.jpg"
   }
+}
+
+
 
 function App() {
 
-  const [userChoice, setUserChoice] = useState(null);
+  const [userChoice, setUserChoice] = useState(null)
 
   const play = (userSelect)=>{
-    // setUserChoice
-    console.log(userSelect);
     setUserChoice(choice[userSelect]);
   }
 
   return (
     <div className='wrapper'>
-      <div className='main'>
+      <div className="main">
         <Box title="You" item={userChoice}/>
         <Box title="Computer"/>
       </div>
-      <div className='main'>
+      <div className="main">
         <button onClick={()=>play("scissors")}>가위</button>
         <button onClick={()=>play("rock")}>바위</button>
         <button onClick={()=>play("paper")}>보</button>
       </div>
     </div>
-  
+
   )
 }
 
