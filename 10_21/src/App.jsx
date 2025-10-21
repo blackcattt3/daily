71c96bd5a014a69beaf8b65ac4bd3cb08160af24
Jaehee Roh <snowcat3b@gmail.com>
2025-10-21 20:47:51 +0900

    box color change

---
+++ 10_21/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Box from './component/Box'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Box/>
+    </div>
+  )
+}
+
+export default App
---
+++ 10_21/src/component/Box.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Box.css'
+import { useEffect, useState, useRef } from 'react'
+
+const Box = () => {
+    const [isClick, setIsClick] = useState(false);
+    const [smallBoxClick, setSmallBoxClick] = useState(false)
+    const boxRef = useRef(null)
+
+    useEffect(()=>{
+        const outSideClick = (e)=>{
+            // e.current가 button 내에 있는지 없는지 판단할것.
+            if(isClick && boxRef.current && boxRef.current.contains(e.target)){
+                setIsClick(false);
+                console.log(e.target)
+        }}
+        if(isClick){
+            document.addEventListener('click', outSideClick)
+            console.log("해제준비")
+        }
+        return ()=>{
+            document.removeEventListener('click', outSideClick)
+            console.log("이벤트 제거")
+        }
+    },[isClick])
+
+
+  return (
+    <div>
+        <div className={`box ${isClick? "purple":""}`} onClick={(e)=>{e.stopPropagation();setIsClick(!isClick)}}></div>
+        <div ref={boxRef} className='button'>A</div>
+
+        <div className={`small-box ${smallBoxClick?"smallColor":""}`}></div>
+        <button onClick={()=>{setSmallBoxClick(!smallBoxClick)}}>change</button>
+    </div>
+    
+  )
+}
+
+export default Box
